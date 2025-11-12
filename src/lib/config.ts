2afe9d0e42dbb8a494bed9cb1e2502182006f39d
wpinocho <julian.ruiz.loza@gmail.com>
2025-11-12 13:51:39 -0600

    [lovivo-provision-config] Configure store settings and Stripe keys

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,3 @@
-export const STORE_ID = "7643ff1a-3609-44af-aa55-2d445087f5bc";
-export const STRIPE_PUBLISHABLE_KEY =
-  "pk_live_51Rk9SNP66p9BDoW4lgxDnuqvNZYEaL400FCX0GStvuaslkEMBZNRqln5M5bDcBSBmFmR7hlY0pDcA4u4VIi8CRIg00KkqRzlVs";
-export const LOVIVO_STRIPE_ACCOUNT_ID = "acct_1Rk9SNP66p9BDoW4";
+export const STORE_ID = 'fec851fc-faff-4f7a-9f2a-80dbc561ac28'
+export const STRIPE_PUBLISHABLE_KEY = 'pk_live_51Rk9SNP66p9BDoW4lgxDnuqvNZYEaL400FCX0GStvuaslkEMBZNRqln5M5bDcBSBmFmR7hlY0pDcA4u4VIi8CRIg00KkqRzlVs'
+export const LOVIVO_STRIPE_ACCOUNT_ID = 'acct_1Rk9SNP66p9BDoW4'
